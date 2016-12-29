1909049f921c7403cb2249072833367e49d2956d
Sangam Mahawar <isangam9@gmail.com>
2016-12-29 12:22:04 -0800

    Add files via upload

---
+++ js/blood.js
@@ -0,0 +1,12 @@
+onload = function(){var acc = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+acc[0].click();
+}
